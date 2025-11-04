ad6088e28da5386a5ade68d8bd72656ad69d0620
17Gaurav01 <60849752+17Gaurav01@users.noreply.github.com>
2025-11-04 11:16:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.cpp
@@ -0,0 +1,40 @@
+/*
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int cycleStart(Node* head) {
+        // code here
+        Node *i=head;
+        Node *j=head;
+        
+        int f=0;
+        while(j and j->next){
+            i=i->next;
+            j=j->next->next;
+            if(i==j){
+                f=1;
+                break;
+            }
+        }
+        if(f==0) return -1;
+        j=head;
+        
+        while(i!=j){
+            i=i->next;
+            j=j->next;
+        }
+        return i->data;
+        
+    }
+};
