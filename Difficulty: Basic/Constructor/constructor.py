609cc4a676e55409e4b34e9b31cda6704a0c7ba9
17Gaurav01 <60849752+17Gaurav01@users.noreply.github.com>
2026-04-23 10:07:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Constructor/constructor.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+# Driver code will print instance variable like this
+# user = User()
+# print(user.name)
+
+class User:
+    name=""
+    def __init__(self):
+        self.name="Default"
+        
