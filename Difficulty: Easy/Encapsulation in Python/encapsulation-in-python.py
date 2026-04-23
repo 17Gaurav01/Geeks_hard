99a5a7a329a3c5b9edbbb0126e634985cbbbcac6
17Gaurav01 <60849752+17Gaurav01@users.noreply.github.com>
2026-04-23 17:57:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Encapsulation in Python/encapsulation-in-python.py
+++ Difficulty: Easy/Encapsulation in Python/encapsulation-in-python.py
@@ -1,18 +1,18 @@
 #User function Template for python3
 
 # Define the Person class
+
 class Person:
-    def __init__(self,name = "Geeks",age = 10):
-        self.__name = name
-        self.__age = age
-        
-    def get_name(self):
-        return self.__name
+    def __init__(self,name="Geeks",age=10):
+        self.name=name
+        self.age=age
+    
     def set_name(self,name):
-        self.__name=name
-        
+        self.name=name
     def set_age(self,age):
-        self.__age=age
+        self.age=age
+    
     def get_age(self):
-        return self.__age
-        
+        return self.age
+    def get_name(self):
+        return self.name
